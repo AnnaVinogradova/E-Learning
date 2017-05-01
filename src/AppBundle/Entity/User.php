20e7c23b8c44593fad20626c8b1e6f4c94915b45
Anna Vinogradova <vinogradova.hanna@gmail.com>
2017-05-01 13:16:14 +0300

    Add FOSUserBundle and Doctrine Migrations to project
    - Add bundle, config, routing, security part, User Entity
    - Add Doctrine Migrstions

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 5/1/17
+ * Time: 12:05 PM
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table("fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
