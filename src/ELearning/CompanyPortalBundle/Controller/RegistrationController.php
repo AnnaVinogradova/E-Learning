0f3109773c6ca8aa8210820317f8329389d38d40
Anna Vinogradova <vinogradova.hanna@gmail.com>
2017-05-07 12:55:42 +0300

    Add new form class and action for Company registration

--- src/ELearning/CompanyPortalBundle/Controller/RegistrationController.php
+++ src/ELearning/CompanyPortalBundle/Controller/RegistrationController.php
@@ -8,12 +8,41 @@
 
 namespace ELearning\CompanyPortalBundle\Controller;
 
+use ELearning\CompanyPortalBundle\Form\CompanyRegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationController extends Controller
 {
     public function indexAction()
     {
         return $this->render('PortalBundle:Registration:index.html.twig');
+    }
+
+    public function registerAction(Request $request)
+    {
+        $form = $this->createForm(CompanyRegistrationType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $userManager = $this->get('fos_user.user_manager');
+            $email_exist = $userManager->findUserByEmail($data["email"]);
+            $username_exist = $userManager->findUserByUsername($data["username"]);
+            if(!$email_exist && !$username_exist){
+                $user = $userManager->createUser();
+                $user->setUsername($data["username"]);
+                $user->setEmail($data["email"]);
+                $user->setEmailCanonical($data["email"]);
+                $user->setEnabled(1);
+                $user->setPlainPassword($data["password"]);
+                $userManager->updateUser($user);
+            }
+        }
+
+        return $this->render('PortalBundle:Registration:register.html.twig', array(
+            'form' => $form->createView()
+    ));
     }
 }
---
+++ src/ELearning/CompanyPortalBundle/Form/CompanyRegistrationType.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 5/7/17
+ * Time: 12:02 PM
+ */
+namespace ELearning\CompanyPortalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CompanyRegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', 'text')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
